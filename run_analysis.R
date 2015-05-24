a0db3d03c022dc8755086ccd41a3c82eb5fcea76
Douvid <dkrelman@gmail.com>
2015-05-25 00:02:38 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -15,7 +15,7 @@ run_analysis <- function (FolderPath){
       ReadData3 <- read.table("train/subject_train.txt", header=FALSE)
       colnames(ReadData3) <- c("Subject") # naming the column Subject
       DataFrame1 <- cbind(ReadData3,ReadData2,ReadData1) # combining the 
-      names(DataFrame1)[2]<-"Activity"
+      names(DataFrame1)[2]<-"Activity" # naming the second column Activity
       
       ReadData4 <- read.table("test/X_test.txt", header=FALSE)
       colnames(ReadData4) <- Headers[,2]
@@ -25,11 +25,11 @@ run_analysis <- function (FolderPath){
       ReadData6 <- read.table("test/subject_test.txt", header=FALSE)
       colnames(ReadData6) <- c("Subject")
       DataFrame2 <- cbind(ReadData6,ReadData5,ReadData4)
-      names(DataFrame2)[2]<-"Activity"
+      names(DataFrame2)[2]<-"Activity" # naming the second column Activity
       DataFrame <- rbind(DataFrame1,DataFrame2) # merge the two dataframe 1- 2 to provide a unique file with headers
       
       
       DataFrameClean <- DataFrame[,c("Subject","Activity",colnames(DataFrame)[grep("std()|mean()",colnames(DataFrame))])] # filtering the data.frame to keep only values of mean/stand
       dt <- DataFrameClean %>% group_by(Subject, Activity) %>% summarise_each(funs(mean))
-      write.table(dt, "Run_Analysis_SmartPhone.txt", row.names=FALSE)
+      write.table(dt, "Run_Analysis_SmartPhone.txt", row.names=FALSE) #exporting the results to a new file
 }
